be5ec741f00bd1fb333777d9bf78f6a716a04f6c
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-02-12 23:34:36 +0530

    Fix #5 Add pre-commit to format all JS files for indentation. (#9)
    * Fix #5 Add pre-commit to format all JS files for indentation.
    
    * Adding husky pre-commmit  hook.
    
    * minor change
    
    * minor changes
    
    * Adding pre commit
    
    * minor change
    
    * minor changes.
    
    * Removed lint-staged package
    
    * Minor changes
    
    * minor changes
    
    * added husky and lint-stagged
    
    * added husky and lint-stagged

--- src/lib/shapes/base.tsx
+++ src/lib/shapes/base.tsx
@@ -1,57 +1,66 @@
 import React from 'react';
 import { ResponsiveShape } from '../helpers/responsive';
-import {expandValues} from '../helpers/utils';
+import { expandValues } from '../helpers/utils';
 import { checkDim, checkNonDecreasing } from '../helpers/error';
+import { ReactNode } from 'react';
 
 type BaseShapeProps = {
-    height: string | string[]
-    width: string | string[]
-    top?: string | string[]
-    left?: string | string[]
-    right?: string | string[]
-    bottom?: string | string[]
-    zIndex?: string
-    className?: string
-    children: any
-    breakpoints?: number[]
-    position?: string
-}
+  height: string | string[];
+  width: string | string[];
+  top?: string | string[];
+  left?: string | string[];
+  right?: string | string[];
+  bottom?: string | string[];
+  zIndex?: string;
+  className?: string;
+  children: ReactNode;
+  breakpoints?: number[];
+  position?: string;
+};
 
 export const BaseShape = ({
-        children,
-        height,
-        width,
-        position=`absolute`,
-        top=``,
-        left=``,
-        right=``,
-        bottom=``,
-        className=`anim-shapes`,
-        zIndex=`-1`,
-        breakpoints=[600, 900, 1200]}: BaseShapeProps) => {
-    
-    if(!checkNonDecreasing(breakpoints) ||
-       !checkDim(top, breakpoints.length) ||
-       !checkDim(left, breakpoints.length) ||
-       !checkDim(right, breakpoints.length) ||
-       !checkDim(bottom, breakpoints.length)){
-        return <></>;
-    }
-    let responsiveHeight = expandValues(height, breakpoints.length);
-    let responsiveWidth = expandValues(width, breakpoints.length);
-    let responsiveTop = expandValues(top, breakpoints.length);
-    let responsiveLeft = expandValues(left, breakpoints.length);
-    let responsiveRight = expandValues(right, breakpoints.length);
-    let responsiveBottom = expandValues(bottom, breakpoints.length);
-    return <ResponsiveShape
-                height={responsiveHeight}
-                width={responsiveWidth}
-                top={responsiveTop}
-                left={responsiveLeft}
-                right={responsiveRight}
-                bottom={responsiveBottom}
-                position={position}
-                className={className}
-                zIndex={zIndex}
-                breakpoints={breakpoints}>{children}</ResponsiveShape>;
-}
+  children,
+  height,
+  width,
+  position = `absolute`,
+  top = ``,
+  left = ``,
+  right = ``,
+  bottom = ``,
+  className = `anim-shapes`,
+  zIndex = `-1`,
+  breakpoints = [600, 900, 1200]
+}: BaseShapeProps) => {
+  if (
+    !checkNonDecreasing(breakpoints) ||
+    !checkDim(top, breakpoints.length) ||
+    !checkDim(left, breakpoints.length) ||
+    !checkDim(right, breakpoints.length) ||
+    !checkDim(bottom, breakpoints.length)
+  ) {
+    return <></>;
+  }
+  const responsiveHeight = expandValues(height, breakpoints.length);
+  const responsiveWidth = expandValues(width, breakpoints.length);
+  const responsiveTop = expandValues(top, breakpoints.length);
+  const responsiveLeft = expandValues(left, breakpoints.length);
+  const responsiveRight = expandValues(right, breakpoints.length);
+  const responsiveBottom = expandValues(bottom, breakpoints.length);
+
+  return (
+    <ResponsiveShape
+      bottom={responsiveBottom}
+      breakpoints={breakpoints}
+      className={className}
+      height={responsiveHeight}
+      left={responsiveLeft}
+      position={position}
+      right={responsiveRight}
+      top={responsiveTop}
+      width={responsiveWidth}
+      zIndex={zIndex}
+    >
+      {children}
+    </ResponsiveShape>
+  );
+};
