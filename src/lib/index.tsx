1751f1623abc41fa351b4080965d92bee09335a4
mohittk <mvk1407@gmail.com>
2022-02-13 09:59:39 +0530

    added square and diamond

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,5 @@
 export { Donut } from './shapes/donut';
 export { Circle } from './shapes/circle';
 export { CircleGrid } from './shapes/circlegrid';
+export { Square } from './shapes/square';
+export { Diamond } from './shapes/diamond';
