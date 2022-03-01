68d620b7743b4a314b2a91493d054b28f08cea7d
Aman Sethi <35420536+amansethi00@users.noreply.github.com>
2022-03-01 07:35:46 +0530

    remove unused test and webvitals files and refactor imports to keep absolute imports on top followed by relative imports (#38)

--- src/lib/helpers/responsive.tsx
+++ src/lib/helpers/responsive.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { useMediaQuery } from 'react-responsive';
+import styled from '@emotion/styled';
 
 type ResponsiveShapeProps = {
   height: string[];
