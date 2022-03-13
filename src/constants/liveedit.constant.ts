daa952d76a7f6a3a72f7f434fada63b0e21ef888
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2022-03-13 19:56:13 +0530

    Code CleanUp and Refactoring Part 2 (#76)

---
+++ src/constants/liveedit.constant.ts
@@ -0,0 +1,5 @@
+export const foreground = '#f8f8f2';
+export const errorBackground = '#ff5555';
+
+export const codeBackground = '#011727';
+export const previewBackground = '#eef2ff';
---
+++ src/constants/social.constant.ts
@@ -0,0 +1,2 @@
+export const discordLink = 'https://discord.gg/GkcbM5bwZr';
+export const githubLink = 'https://github.com/ashutosh1919/react-awesome-shapes';
