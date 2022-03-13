7d54c3e91e2a6ed0ccfea8e25c1f063a863f05aa
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-03-13 11:08:47 +0530

    Fix: issue-45 diamond to square (#70)
    * Fix: issue-45 diamond to square
    
    * Addressing comments
    
    * reducing size of diamond shape

--- src/components/shapes.tsx
+++ src/components/shapes.tsx
@@ -44,6 +44,7 @@ export const Shapes = () => {
         color="linear-gradient(225deg, #fed7aa, #fb923c)"
         right={[`40px`, `50px`, `60px`, `120px`]}
         size="30px"
+        sx={{ animation: `${upKeyFrame} 6s ease-in-out infinite alternate` }}
         top="100px"
       />
 
