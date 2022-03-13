9c97d722c8ccee73d5dcad7aa63e95822144a271
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2022-03-13 10:58:24 +0530

    reducing size of diamond shape

--- src/components/shapes.tsx
+++ src/components/shapes.tsx
@@ -43,7 +43,7 @@ export const Shapes = () => {
       <Diamond
         color="linear-gradient(225deg, #fed7aa, #fb923c)"
         right={[`40px`, `50px`, `60px`, `120px`]}
-        size="50px"
+        size="30px"
         sx={{ animation: `${upKeyFrame} 6s ease-in-out infinite alternate` }}
         top="100px"
       />
